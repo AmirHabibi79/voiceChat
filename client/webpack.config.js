eb8501e4415f9af686718220033117573939a1f9
AmirHabibi79 <www.habibiamir883@gmail.com>
2024-07-16 21:29:39 +0330

    Make some changes to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,42 +1,55 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./index.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: "single",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "voice chat",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
+const webpack = require("webpack");
 
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        include: path.resolve(__dirname, "src"),
-      },
+module.exports = (env, args) => {
+  const SERVER_URL =
+    env.IS_HTTPS === "true"
+      ? JSON.stringify("https://" + env.SERVER_DOMAIN + ":" + env.SERVER_PORT)
+      : JSON.stringify("http://" + env.SERVER_DOMAIN + ":" + env.SERVER_PORT);
+  return {
+    mode: "development",
+    entry: {
+      index: "./index.js",
+    },
+    devtool: args.mode === "development" ? "inline-source-map" : "source-map",
+    devServer: {
+      static: "./dist",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    optimization: {
+      runtimeChunk: "single",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "voice chat",
+      }),
+      new webpack.DefinePlugin({
+        "process.env": {
+          SERVER_URL,
+        },
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          include: path.resolve(__dirname, "src"),
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+  };
 };
